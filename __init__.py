d1c920710e83b238042c569e273260b9ca39d44d
Tanawat Thuamthet <tanawat-th@nctc.cgn.nidec-copal.co.jp>
2024-02-26 10:51:10 +0700

    edit path

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 #from keras.models import load_model
 
+path_route = "/AI/"
 
 #start server
 app = Flask(__name__)
@@ -11,7 +12,7 @@ CORS(app)
 #load model
 #smodel = load_model("./model/keras_model.h5", compile=False)
 
-@app.route('/AI/HELLO', methods=['GET'])
+@app.route(f'{path_route}HELLO', methods=['GET'])
 def getHello():
     return jsonify({'status': 'success', 'data': 'hello'})
 
