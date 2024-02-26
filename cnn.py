7c4176df10fbd1ab884dac4c046ca1c0dee412a5
Mintorn Taunhar <Mintorn-ta@nctc.cgn.nidec-copal.co.jp>
2024-02-26 15:39:24 +0700

    edit opencv to pillow

--- cnn.py
+++ cnn.py
@@ -1,7 +1,7 @@
 from keras.models import load_model  # TensorFlow is required for Keras to work
 import cv2  # Install opencv-python
 import numpy as np
-
+from PIL import Image, ImageOps
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
 
@@ -12,22 +12,27 @@ class cnn:
 
     def detech(self, file):
         print(file)
-        image = cv2.resize(file, (224, 224), interpolation=cv2.INTER_AREA)
+        with Image.open(file) as image:
+            size = (224, 224)
+            image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
 
-        # Make the image a numpy array and reshape it to the models input shape.
-        image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+            # turn the image into a numpy array
+            image_array = np.asarray(image)
+            data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+            # Normalize the image
+            normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+           
+            # Load the image into the array
+            data[0] = normalized_image_array
 
-        # Normalize the image array
-        image = (image / 127.5) - 1
+            # Predicts the model
+            prediction = self.model.predict(data)
+            index = np.argmax(prediction)
+            class_name = self.class_names[index]
+            confidence_score = prediction[0][index]
 
-        # Predicts the model
-        prediction = self.model.predict(image)
-        index = np.argmax(prediction)
-        class_name = self.class_names[index]
-        confidence_score = prediction[0][index]
-
-        # Print prediction and confidence score
-        print("Class:", class_name[2:], end="")
-        print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+            # Print prediction and confidence score
+            print("Class:", class_name[2:], end="")
+            print("Confidence Score:", confidence_score)
 
         return [class_name[2:],str(np.round(confidence_score * 100))[:-2]]
