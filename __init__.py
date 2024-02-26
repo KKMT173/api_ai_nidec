421b0029c4af57a1a14ff8df35760697c33096dc
Tanawat Thuamthet <tanawat-th@nctc.cgn.nidec-copal.co.jp>
2024-02-26 10:56:04 +0700

    add path ai cnn

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-#from keras.models import load_model
+from keras.models import load_model
 
 path_route = "/AI/"
 
@@ -10,11 +10,19 @@ app = Flask(__name__)
 CORS(app)
 
 #load model
-#smodel = load_model("./model/keras_model.h5", compile=False)
+model = load_model("./model/keras_model.h5", compile=False)
 
 @app.route(f'{path_route}HELLO', methods=['GET'])
 def getHello():
     return jsonify({'status': 'success', 'data': 'hello'})
+
+@app.route(f'{path_route}CNN', methods=['POST'])
+def process_file_blank():
+    response = {}
+    if request.files['file']:
+        file = request.files['file']
+        response = class.run(model,file)
+    return jsonify({'status': 'successful', 'data': response})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port='3545',debug=True)
