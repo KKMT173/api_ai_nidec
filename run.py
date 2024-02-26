7f5b325f0c65d7243102c56cddddfb41f0270f9b
Tanawat Thuamthet <tanawat-th@nctc.cgn.nidec-copal.co.jp>
2024-02-26 11:01:22 +0700

    edit file cnn

--- run.py
+++ run.py
@@ -5,47 +5,28 @@ import numpy as np
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
 
-# Load the model
-model = load_model("keras_Model.h5", compile=False)
+class cnn:
+    def __init__(self):
+        pass
 
-# Load the labels
-class_names = open("labels.txt", "r").readlines()
+    def detech(self, model, file):
 
-# CAMERA can be 0 or 1 based on default camera of your computer
-camera = cv2.VideoCapture(0)
+        image = cv2.resize(file, (224, 224), interpolation=cv2.INTER_AREA)
 
-while True:
-    # Grab the webcamera's image.
-    ret, image = camera.read()
+        # Make the image a numpy array and reshape it to the models input shape.
+        image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
 
-    # Resize the raw image into (224-height,224-width) pixels
-    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+        # Normalize the image array
+        image = (image / 127.5) - 1
 
-    # Show the image in a window
-    cv2.imshow("Webcam Image", image)
+        # Predicts the model
+        prediction = model.predict(image)
+        index = np.argmax(prediction)
+        class_name = class_names[index]
+        confidence_score = prediction[0][index]
 
-    # Make the image a numpy array and reshape it to the models input shape.
-    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+        # Print prediction and confidence score
+        print("Class:", class_name[2:], end="")
+        print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
 
-    # Normalize the image array
-    image = (image / 127.5) - 1
-
-    # Predicts the model
-    prediction = model.predict(image)
-    index = np.argmax(prediction)
-    class_name = class_names[index]
-    confidence_score = prediction[0][index]
-
-    # Print prediction and confidence score
-    print("Class:", class_name[2:], end="")
-    print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
-
-    # Listen to the keyboard for presses.
-    keyboard_input = cv2.waitKey(1)
-
-    # 27 is the ASCII for the esc key on your keyboard.
-    if keyboard_input == 27:
-        break
-
-camera.release()
-cv2.destroyAllWindows()
+        return [class_name[2:],str(np.round(confidence_score * 100))[:-2]]
