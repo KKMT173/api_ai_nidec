59a6930f59d5af7cbbc91f65cca6d8d67cc7e39a
Mintorn Taunhar <Mintorn-ta@nctc.cgn.nidec-copal.co.jp>
2024-02-28 08:25:01 +0700

    1stcommitdev-find

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from keras.models import load_model
 from io import BytesIO
 from cnn import cnn
-
+from sever_find_blank import sever_find_blank
 path_route = "/AI/"
 
 #start server
@@ -20,7 +20,7 @@ def getHello():
     return jsonify({'status': 'success', 'data': 'hello'})
 
 @app.route(f'{path_route}CNN', methods=['POST'])
-def process_file_blank():
+def process_cnn():
     global class_names
     print(class_names)
     response = {}
@@ -32,5 +32,16 @@ def process_file_blank():
         response = model_cnn.detech(BytesIO(file_data))
     return jsonify({'status': 'successful', 'data': response})
 
+@app.route(f'{path_route}server_find_blank', methods=['POST'])
+def process_find_blank():
+    stamp = request.form['stamp']
+	file = request.files['file']
+	file_data = file.read()
+	output_path = sever_find_blank.find_and_replace_blank(BytesIO(file_data), stamp)
+	with open(output_path, 'rb') as output_file:
+	output_data = output_file.read()
+	response = jsonify({'status': 'success', 'base64_img': 'data:image/png;base64,'+base64.b64encode(output_data).decode('utf-8')})
+	return response
+	
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port='3545',debug=True)
--- server_find_blank.py
+++ server_find_blank.py
@@ -7,86 +7,56 @@ import random
 app = Flask(__name__)
 CORS(app)
 
-
-def find_blank_area(img, stamp_width, stamp_height):
-    # ค้นหาพื้นที่ว่างในรูปภาพ
-    width, height = img.size
-    # print(width,height)
-    for x in range(0, width - stamp_width, random.choice(range(3,10))):
-        print(x)
-        for y in range(0, height - stamp_height + 1, 5):
-           
-            region = img.crop((x, y, x + stamp_width, y + stamp_height))
-            region_data = list(region.getdata())
-            # ตรวจสอบว่าพื้นที่นี้ว่างหรือไม่
-            is_blank = all(pixel[2] == 255 for pixel in region_data)
-
-
-            if is_blank:
-                # print("TEST in find blank")
-                return (x, y)
-            
-    # ถ้าไม่พบพื้นที่ว่าง
-    return None
-
-def find_and_replace_blank(image_path, stamp):
-    # โปรแกรมนี้ใช้ Pillow เพื่อประมวลผลรูปภาพ
-
-    with Image.open(image_path) as img:
-
-        for stamp_value in stamp.split(','):
-            stamp_img = Image.open('./picture_stamp/'+stamp_value)
-
-            width, height = stamp_img.size
-
-            # ประมวลผลหาตำแหน่งที่จะใส่ stamp และทำการใส่ stamp
-            # (โปรดปรับปรุงโค้ดตามความต้องการ)
-            x = 0
-            y = 0
-            blank_area = find_blank_area(img, width, height)
-
-            if blank_area is not None:
-                # ทำการใส่ stamp ลงไปในพื้นที่ว่าง
-                img.paste(stamp_img, blank_area, stamp_img)
-
-    output_path = 'output_image.png'
-    img.save(output_path)
-
-    return output_path
-
-@app.route('/AI/TEST', methods=['GET'])
-def test():
-    response = jsonify({'status': 'success', 'text': 'hello'})
-    return response
-
-@app.route('/AI/file_blank', methods=['POST'])
-def process_file_blank():
-
-    if True:
-
-        # รับ stamp และ file จาก body form
-        stamp = request.form['stamp']
-        file = request.files['file']
-
-        # อ่านข้อมูลจากไฟล์รูปภาพ
-        file_data = file.read()
-
-        # ทำการประมวลผลและใส่ stamp
-
-        output_path = find_and_replace_blank(BytesIO(file_data), stamp)
+class sever_find_blank:
+    """docstring for sever_find_blank"""
+    def __init__(self):
+        pass
+        
+    def find_blank_area(self, img, stamp_width, stamp_height):
+        # ค้นหาพื้นที่ว่างในรูปภาพ
+        width, height = img.size
+        # print(width,height)
+        for x in range(0, width - stamp_width, random.choice(range(3,10))):
+            print(x)
+            for y in range(0, height - stamp_height + 1, 5):
+               
+                region = img.crop((x, y, x + stamp_width, y + stamp_height))
+                region_data = list(region.getdata())
+                # ตรวจสอบว่าพื้นที่นี้ว่างหรือไม่
+                is_blank = all(pixel[2] == 255 for pixel in region_data)
 
 
-        # อ่านไฟล์ที่ได้หลังจากประมวลผล
-        with open(output_path, 'rb') as output_file:
-            output_data = output_file.read()
+                if is_blank:
+                    # print("TEST in find blank")
+                    return (x, y)
+                
+        # ถ้าไม่พบพื้นที่ว่าง
+        return None
 
-        # ส่งไฟล์ที่ประมวลผลแล้วกลับไปยังไคลเอนต์
-        response = jsonify({'status': 'success', 'base64_img': 'data:image/png;base64,'+base64.b64encode(output_data).decode('utf-8')})
-        return response
-    """
-    except Exception as e:
-        return jsonify({'status': 'error', 'message': str(e)})
-    """
+    def find_and_replace_blank(self, image_path, stamp):
+        # โปรแกรมนี้ใช้ Pillow เพื่อประมวลผลรูปภาพ
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0",port='3545',debug=True)
+        with Image.open(image_path) as img:
+
+            for stamp_value in stamp.split(','):
+                stamp_img = Image.open('./picture_stamp/'+stamp_value)
+
+                width, height = stamp_img.size
+
+                # ประมวลผลหาตำแหน่งที่จะใส่ stamp และทำการใส่ stamp
+                # (โปรดปรับปรุงโค้ดตามความต้องการ)
+                x = 0
+                y = 0
+                blank_area = self.find_blank_area(img, width, height)
+
+                if blank_area is not None:
+                    # ทำการใส่ stamp ลงไปในพื้นที่ว่าง
+                    img.paste(stamp_img, blank_area, stamp_img)
+
+        output_path = 'output_image.png'
+        img.save(output_path)
+
+        return output_path
+
+
+   
